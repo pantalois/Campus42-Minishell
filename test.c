4b75d666c91a5f92e163015dcc67bf320ba77454
Lois González  Alonso <loigonza@car10s6.42barcelona.com>
2024-11-30 12:13:19 +0100

    30/11

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: loigonza <loigonza@42.barcel>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 12:00:22 by loigonza          #+#    #+#             */
-/*   Updated: 2024/11/21 12:38:53 by loigonza         ###   ########.fr       */
+/*   Updated: 2024/11/30 12:12:35 by loigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,47 @@
 #include <unistd.h>
 
 
-int	ft_check_builtin(char *line)
+/*int	ft_check_builtin(char *line)
 {
-
-}
+	
+}*/
 
 int main(void)
 {
 	char *line;
+	char *historial;
+
+	historial = NULL;
 	
 	while (1)
 	{	
 		line = readline("Minicaracola: "); //Aquí el programa se para hasta pulsar el intro, y continua el código.
 		//Esta linea la recive Gerri en su parseo, y me la devuelve spliteada.
 		//En el add_history tenemos que controlar que entra y que no.
-		if (line)
-			add_history(line);
-		ft_check_builtin(line);//Pongo line para hacer test, pero debería hacer comprobación con la primera string retornada del parseo.	
+	
+		
+
+		if (line == NULL) //Caso en el que el usuario entra CTRL+D
+		{
+			break ;
+		}
+		if (line[0] != '\0') //Comprueba si no ha sido presionado intro
+		{
+			if (!historial) //Comprobar si hemos hecho la  primera iteracion con la terminal
+			{		
+				historial = line; //Guardar la primera iteracion
+				add_history(line); 
+			}
+			else //En caso de que ya tengamos algo en el historial
+			{
+				if (ft_strncmp(historial, line, ft_strlen(historial))) //Comprueba si el elemento anterior en el historial es el mismo que el actual
+				{
+					historial = line;
+					add_history(line);
+				}
+			}
+		}
+		//ft_check_builtin(line);//Pongo line para hacer test, pero debería hacer comprobación con la primera string retornada del parseo.	
 	}	
 	return (0);
 }
